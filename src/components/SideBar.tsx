9aa7fee8d6092b80a26a0347a1cdfa1daa1cb35a
Navar Kartalian <alpha.hunter88@gmail.com>
2021-03-16 17:02:28 -0300

    Components Created

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps, MovieProps } from '../App'
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  setSelectedGenreId(id: number): void;
+
+}
+
+export function SideBar(props: SideBarProps) {
+  const {genres, selectedGenreId, setSelectedGenreId} = props;
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return(
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map((genre: {id: any; title: any; name: any;}) => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>
+  );
 }
